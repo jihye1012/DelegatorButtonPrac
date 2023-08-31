2e5960ceaa4641125bd3d17a307d2685485c80a5
Jihye Yang <s2127@e-mirim.hs.kr>
2023-08-31 15:39:39 +0900

    초기 커밋.

---
+++ DelegatorButtonPrac/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegatorButtonPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button button = new Button();
+            button.Text = "버튼";
+            button.Click += delegate (object s, EventArgs a)
+            {
+                MessageBox.Show("무명 델리게이터를 사용한 이벤트 연결");
+
+            };
+            EventHandler click = null;
+
+            button.Click += (e2, a2) =>
+            {
+                MessageBox.Show("람다를 사용한 이벤트 연결");
+                button.Click -= click;
+              
+            };
+            button.Click += click;
+            Controls.Add(button);
+        }
+    }
+}
